8199eb481a86a9dc86ee3b16ec9a319a94a1ab6b
Thomas-Gustafsson <90902871+Thomas-Gustafsson@users.noreply.github.com>
2022-02-01 14:43:24 +0100

    Update timetemplate.asm

--- Laborationer/Lab1/timetemplate.asm
+++ Laborationer/Lab1/timetemplate.asm
@@ -74,4 +74,21 @@ tiend:	sw	$t0,0($a0)	# save updated result
 	nop
 
   # you can write your code for subroutine "hexasc" below this line
-  #
+hexasc:
+	andi 	$a0,$a0, 0xf 		# to keep the 4 LSB in the argument
+	
+	ble 	$a0, 0x9, number	# branch if the input is less than or equal to 9, else continue
+	nop				# delay slot filler (just in case)
+	
+	ble 	$a0, 0xf, char		# branch if the input if less than or equal to 15 (but greater than 9)
+	nop				# delay slot filer (just in case)
+	
+	number:				# 0-9
+	addi	$v0, $a0, 0x30		# ASCII numbers start at 0x30 (0), so add $a0 to that for correct output 
+	jr	$ra			
+	nop
+	
+	char:				# 10-15
+	addi	$v0, $a0, 0x37		# ASCII letters start at 0x41 (A)
+	jr	$ra
+	nop
