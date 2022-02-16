ac495ef8dbf05db7ed50363babe782957315237b
Thomas-Gustafsson <90902871+Thomas-Gustafsson@users.noreply.github.com>
2022-02-16 20:47:58 +0100

    Update analyse.asm

--- Laborationer/Lab1/analyse.asm
+++ Laborationer/Lab1/analyse.asm
@@ -1,21 +1,19 @@
   # analyze.asm
-  # This file written 2015 by F Lundevall
+  # This file written 2015 by F Lundevall. Edited 2022 by T Gustafsson
   # Copyright abandoned - this file is in the public domain.
-  # Edited 2022 by T Gustafsson
 
 	.text
 main:
 	li	$s0,0x30	# ASCII 0x30 = 0. Char 0 loads into s0
-loop:				# loop starts here
-	move	$a0,$s0		# copy from s0 to a0
-	
-	li	$v0,11		# syscall with v0 = 11 will print out
-	syscall			# one byte from a0 to the Run I/O window
+loop:
+	move	$a0,$s0		# copy contents of s0 to a0 
 
-	addi	$s0,$s0,3	# s0 is set to s0 but with overflow 3, every third char
-				# will print after code below
+	li	$v0,11		# function in SYSCALL system services: (v0, 11) a0 = char to print
+	syscall			# will print char contained in a0 to Run I/O window
+
+	addi	$s0,$s0,3	# adding immidiate value  3 to s0. CHANGED from 1 to 3
 	
-	li	$t0,0x5d	# 0x5b ([) changed to 0x5d (]). Load ] into t0.
+	li	$t0,0x5d	# 0x5b ([) to 0x5d (]).
 	bne	$s0,$t0,loop	# If s0 not equal to t0 0x5d (]), loop. Else stop.
 	
 	nop			# delay slot filler (just in case)
