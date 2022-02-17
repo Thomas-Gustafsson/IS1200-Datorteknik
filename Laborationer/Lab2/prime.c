86e6df5321602055daafc710d10423d4f30ab83c
Thomas-Gustafsson <90902871+Thomas-Gustafsson@users.noreply.github.com>
2022-02-17 11:18:05 +0100

    Update prime.c

--- Laborationer/Lab2/prime.c
+++ Laborationer/Lab2/prime.c
@@ -1,30 +1,33 @@
 /*
  prime.c
  By David Broman.
- Last modified by Thomas Gustafsson 2022-02-10
+ Last modified by Thomas Gustafsson: 2022-02-10
+ This file is in the public domain.
 */
 
 #include <stdio.h>
+#include <time.h>
 
 int is_prime(int n)
 {
-  int c;  // räknare
+   int r; //räknare
 
-  if(n == 1 || n == 0) // fixar att utskriften inte blir 1 när n är 1 eller 0, de är inte primtal.
-    return 0;
-
-  for (c = 2; c <= n-1; c++) // 2 är det minsta möjliga primtalet, for-loopen kör så länge n är större än 2.
-  {
-    if (n%c==0) // om c divideras med den givna siffran n med 0 i rest är n inte ett primtal.
+   if(n == 1 || n == 0)         // kollar om n är 1 eller 0 och printar då ut 0
       return 0;
-  }
-  return 1; // annars är n ett primtal
-}
 
-int main(void){
-  printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
-  printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
-  printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
-  printf("%d\n", is_prime(1)); //1 is not a prime. should print 0
-  printf("%d\n", is_prime(103)); // 103 is a prime.     Should print 1.
+   for(r = 2; r <= n-1; r++)    /*Kollar om n kan delas lika med något tal,
+                                som är mindre eller lika med n-1*/
+   {
+     if(n % r == 0)             // Om resten med efter divison är 0, returnera 0
+     {
+       return 0;
+     }
+   }
+   return 1;                    // Annars returnera 1
+ }
+
+ int main(void){
+   printf("%d\n", is_prime(1));  // 11 is a prime.      Should print 0.
+   printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
+   printf("%d\n", is_prime(0)); // 987 is not a prime. Should print 0.
 }
